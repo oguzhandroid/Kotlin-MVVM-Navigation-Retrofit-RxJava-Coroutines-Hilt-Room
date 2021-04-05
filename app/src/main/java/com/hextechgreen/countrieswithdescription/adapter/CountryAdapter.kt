07dcfef1b314da2ce41c1cc38ebb77a2cd52d0ed
oguzhan <oguzhaneem@gmail.com>
2021-04-05 23:38:49 +0300

    Adapter added

---
+++ app/src/main/java/com/hextechgreen/countrieswithdescription/adapter/CountryAdapter.kt
@@ -0,0 +1,41 @@
+package com.hextechgreen.countrieswithdescription.adapter
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hextechgreen.countrieswithdescription.R
+import com.hextechgreen.countrieswithdescription.model.Country
+
+class CountryAdapter(val countryList : ArrayList<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+    class CountryViewHolder(var view : View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.item_row,parent,false)
+        return CountryViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+
+        holder.view.findViewById<TextView>(R.id.countryName).text = countryList[position].countryName
+        holder.view.findViewById<TextView>(R.id.countryRegion).text = countryList[position].countryRegion
+
+    }
+
+    fun updateCountryList(newCountryList : List<Country>){
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()
+    }
+
+}
